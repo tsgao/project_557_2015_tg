59d4a8c513cac6299df50b5878d279282a2ba26a
tianshuang gao <michael.tsgao@gmail.com>
2015-12-07 14:48:41 -0600

    FinalProject
    Model was loaded

---
+++ FinalProject/SourceCode/CMakeFiles/3.3.1/CompilerIdCXX/CompilerIdCXX.build/Debug/CompilerIdCXX.build/Objects-normal/x86_64/CMakeCXXCompilerId.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/pro/Desktop/Fall2015/COMS557/project_557_2015_tg/FinalProject/SourceCode/CMakeFiles/3.3.1/CompilerIdCXX/CMakeCXXCompilerId.cpp
---
+++ FinalProject/SourceCode/FinalProject.build/Debug/ALL_BUILD.build/Script-318F828DF5F3455E895DDAD5.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+make -C /Users/pro/Desktop/Fall2015/COMS557/project_557_2015_tg/FinalProject/SourceCode -f /Users/pro/Desktop/Fall2015/COMS557/project_557_2015_tg/FinalProject/SourceCode/CMakeScripts/ALL_BUILD_cmakeRulesBuildPhase.make$CONFIGURATION all
